a93a748998c5cd29ce3fd26647eef4e87c7c88e9
Josh Kitchen <joshlkit@gmail.com>
2026-06-04 11:27:12 -0500

    Added main functionality, keys, and json

---
+++ app/keys.py
@@ -0,0 +1,5 @@
+# https://platform.openai.com/settings/organization/api-keys
+OPENAI_API_KEY = ""
+
+# https://portal.azure.com/#@lognpacific.com/resource/subscriptions/3c95e63a-895a-4386-991e-edbbf57de5c8/resourceGroups/Cyber-Range-Admin-SOC/providers/Microsoft.OperationalInsights/workspaces/LAW-Cyber-Range/Overview
+LOG_ANALYTICS_WORKSPACE_ID = "60c7f53e-249a-4077-b68e-55a4ae877d7c"
